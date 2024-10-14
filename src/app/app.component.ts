9e5794c42a302e71f585b1cbeee49992849fd532
Elias Madi <e.madi@deakin.edu.au>
2024-10-14 11:16:45 +1100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,20 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'fbc-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'firebootcamp-crm';
+  title = 'Melbourne firebootcamp-crm';
+  date = new Date();
+
+  changeTitle(){
+    this.title = 'New Title '+Math.random(); 
+  }
+
 }
---
+++ src/app/company-list/company-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'fbc-company-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './company-list.component.html',
+  styleUrl: './company-list.component.scss'
+})
+export class CompanyListComponent {
+
+}
